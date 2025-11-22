4d07fe98b1247ec3e09d7926df01b9fe7eac07ff
jaydoc <jegenk@gmail.com>
2025-11-22 16:25:00 -0600

    mysteriously appearing newline characters changed

--- strokes.js
+++ strokes.js
@@ -1,98 +1,35 @@
-// Stroke data for Tamil letters.
-// SAFE + CLEAN VERSION
-// Each letter has an empty strokes array by default.
-
+// Minimal clean stroke data – no comments, no funny characters.
 window.TAMIL_STROKE_DATA = {
-  "a":  {  // அ
-    "strokes": []
-  },
-  "aa": {  // ஆ
-    "strokes": []
-  },
-  "i":  {  // இ
-    "strokes": []
-  },
-  "ii": {  // ஈ
-    "strokes": []
-  },
-  "u":  {  // உ
-    "strokes": []
-  },
-  "uu": {  // ஊ
-    "strokes": []
-  },
-  "e":  {  // எ
-    "strokes": []
-  },
-  "ee": {  // ஏ
-    "strokes": []
-  },
-  "ai": {  // ஐ
-    "strokes": []
-  },
-  "o":  {  // ஒ
-    "strokes": []
-  },
-  "oo": {  // ஓ
-    "strokes": []
-  },
-  "au": {  // ஔ
-    "strokes": []
-  },
+  "a":  { "strokes": [] },
+  "aa": { "strokes": [] },
+  "i":  { "strokes": [] },
+  "ii": { "strokes": [] },
+  "u":  { "strokes": [] },
+  "uu": { "strokes": [] },
+  "e":  { "strokes": [] },
+  "ee": { "strokes": [] },
+  "ai": { "strokes": [] },
+  "o":  { "strokes": [] },
+  "oo": { "strokes": [] },
+  "au": { "strokes": [] },
 
-  // Mei letters
-  "k":  {  // க்
-    "strokes": []
-  },
-  "ng": {  // ங்
-    "strokes": []
-  },
-  "ch": {  // ச்
-    "strokes": []
-  },
-  "nj": {  // ஞ்
-    "strokes": []
-  },
-  "t":  {  // ட்
-    "strokes": []
-  },
-  "n1": {  // ண்
-    "strokes": []
-  },
-  "th": {  // த்
-    "strokes": []
-  },
-  "n2": {  // ந்
-    "strokes": []
-  },
-  "p":  {  // ப்
-    "strokes": []
-  },
-  "m":  {  // ம்
-    "strokes": []
-  },
-  "y":  {  // ய்
-    "strokes": []
-  },
-  "r1": {  // ர்
-    "strokes": []
-  },
-  "l1": {  // ல்
-    "strokes": []
-  },
-  "v":  {  // வ்
-    "strokes": []
-  },
-  "z":  {  // ழ்
-    "strokes": []
-  },
-  "l2": {  // ள்
-    "strokes": []
-  },
-  "r2": {  // ற்
-    "strokes": []
-  },
-  "n3": {  // ன்
-    "strokes": []
-  }
+  "k":  { "strokes": [] },
+  "ng": { "strokes": [] },
+  "ch": { "strokes": [] },
+  "nj": { "strokes": [] },
+  "t":  { "strokes": [] },
+  "n1": { "strokes": [] },
+  "th": { "strokes": [] },
+  "n2": { "strokes": [] },
+  "p":  { "strokes": [] },
+  "m":  { "strokes": [] },
+  "y":  { "strokes": [] },
+  "r1": { "strokes": [] },
+  "l1": { "strokes": [] },
+  "v":  { "strokes": [] },
+  "z":  { "strokes": [] },
+  "l2": { "strokes": [] },
+  "r2": { "strokes": [] },
+  "n3": { "strokes": [] }
 };
+
