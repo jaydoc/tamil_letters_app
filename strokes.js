5ed8560f07b476c19ecd36ad5db5969531b39a1e
jaydoc <jegenk@gmail.com>
2025-11-23 23:32:31 -0600

    many changes

--- strokes.js
+++ strokes.js
@@ -1,32 +1,39 @@
+// strokes.js
+// Stroke data for Tamil letters. Each key matches the "id" used in index.html.
+// Fill in each `strokes` array from Teacher mode as you trace.
+
 window.TAMIL_STROKE_DATA = {
-  "a":  { "strokes": [] },
-  "aa": { "strokes": [] },
-  "i":  { "strokes": [] },
-  "ii": { "strokes": [] },
-  "u":  { "strokes": [] },
-  "uu": { "strokes": [] },
-  "e":  { "strokes": [] },
-  "ee": { "strokes": [] },
-  "ai": { "strokes": [] },
-  "o":  { "strokes": [] },
-  "oo": { "strokes": [] },
-  "au": { "strokes": [] },
-  "k":  { "strokes": [] },
-  "ng": { "strokes": [] },
-  "ch": { "strokes": [] },
-  "nj": { "strokes": [] },
-  "t":  { "strokes": [] },
-  "n1": { "strokes": [] },
-  "th": { "strokes": [] },
-  "n2": { "strokes": [] },
-  "p":  { "strokes": [] },
-  "m":  { "strokes": [] },
-  "y":  { "strokes": [] },
-  "r1": { "strokes": [] },
-  "l1": { "strokes": [] },
-  "v":  { "strokes": [] },
-  "z":  { "strokes": [] },
-  "l2": { "strokes": [] },
-  "r2": { "strokes": [] },
-  "n3": { "strokes": [] }
+  // Uyir (vowels)
+  "a":  { strokes: [] },  // அ
+  "aa": { strokes: [] },  // ஆ
+  "i":  { strokes: [] },  // இ
+  "ii": { strokes: [] },  // ஈ
+  "u":  { strokes: [] },  // உ
+  "uu": { strokes: [] },  // ஊ
+  "e":  { strokes: [] },  // எ
+  "ee": { strokes: [] },  // ஏ
+  "ai": { strokes: [] },  // ஐ
+  "o":  { strokes: [] },  // ஒ
+  "oo": { strokes: [] },  // ஓ
+  "au": { strokes: [] },  // ஔ
+
+  // Mei (consonants)
+  "k":  { strokes: [] },  // க்
+  "ng": { strokes: [] },  // ங்
+  "ch": { strokes: [] },  // ச்
+  "nj": { strokes: [] },  // ஞ்
+  "t":  { strokes: [] },  // ட்
+  "n1": { strokes: [] },  // ண்
+  "th": { strokes: [] },  // த்
+  "n2": { strokes: [] },  // ந்
+  "p":  { strokes: [] },  // ப்
+  "m":  { strokes: [] },  // ம்
+  "y":  { strokes: [] },  // ய்
+  "r1": { strokes: [] },  // ர்
+  "l1": { strokes: [] },  // ல்
+  "v":  { strokes: [] },  // வ்
+  "z":  { strokes: [] },  // ழ்
+  "l2": { strokes: [] },  // ள்
+  "r2": { strokes: [] },  // ற்
+  "n3": { strokes: [] }   // ன்
 };
